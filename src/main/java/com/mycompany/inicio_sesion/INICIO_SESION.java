600e9135d1647dfd7f40c5c54aa2ed83f69fbb61
luis <luis@DESKTOP-0F46HHJ>
2023-12-29 15:29:19 -0300

    commit 1

---
+++ src/main/java/com/mycompany/inicio_sesion/INICIO_SESION.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.inicio_sesion;
+
+import com.mycompany.inicio_sesion.grafica.Principal;
+
+
+public class INICIO_SESION {
+
+    public static void main(String[] args) {
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+        
+    }
+}
